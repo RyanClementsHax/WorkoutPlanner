c2109cb942aaed733e1248465324a391de319c65
RyanClementsHax <ryanclementsuf@gmail.com>
2017-08-08 13:42:32 -0400

    Initial commit and added recycler view for days of week

---
+++ app/src/main/java/com/example/ryan/workoutplanner/WeekViewActivity.java
@@ -0,0 +1,58 @@
+package com.example.ryan.workoutplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.ryan.workoutplanner.adapters.WeekViewAdapter;
+
+import java.util.Arrays;
+
+public class WeekViewActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerViewAdapter;
+    private RecyclerView.LayoutManager recyclerViewLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView)findViewById(R.id.week_days_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerViewAdapter = new WeekViewAdapter(
+                Arrays.asList(getResources().getStringArray(R.array.days_of_week)),
+                Arrays.asList(getResources().getStringArray(R.array.day_descriptions)));
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_week_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/ryan/workoutplanner/adapters/WeekViewAdapter.java
@@ -0,0 +1,54 @@
+package com.example.ryan.workoutplanner.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ryan.workoutplanner.R;
+
+import java.util.List;
+
+/**
+ * Created by Ryan on 8/8/2017.
+ */
+public class WeekViewAdapter extends RecyclerView.Adapter<WeekViewAdapter.ViewHolder> {
+    private List<String> daysOfWeek;
+    private List<String> dayDescriptions;
+
+    public WeekViewAdapter(List<String> daysOfWeek, List<String> dayDescriptions) {
+        this.daysOfWeek = daysOfWeek;
+        this.dayDescriptions = dayDescriptions;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.day_of_week, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.dayOfWeek.setText(daysOfWeek.get(position));
+        holder.dayDescription.setText(dayDescriptions.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysOfWeek.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView dayOfWeek;
+        TextView dayDescription;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.dayOfWeek = (TextView)itemView.findViewById(R.id.day_of_week);
+            this.dayDescription = (TextView)itemView.findViewById(R.id.day_description);
+        }
+    }
+}
